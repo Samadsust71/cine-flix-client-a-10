23822f08bf305af7c53f6dab5e229898d52f1ba7
Samadsust71 <samadsust71@gmail.com>
2024-12-04 00:45:52 +0600

    added google font

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer'
 
 const RootLayout = () => {
   return (
-    <div >
+    <div className='font-poppins' >
       <header>
         <Header/>
       </header>
