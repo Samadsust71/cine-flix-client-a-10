1da3bc392e9988cd38472e6b2f5f417bc4fd8359
Samadsust71 <samadsust71@gmail.com>
2024-12-08 08:12:47 +0600

    remove no favourite component from favourite movies page

--- src/pages/moviesPages/FavouriteMovies.jsx
+++ src/pages/moviesPages/FavouriteMovies.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../provider/AuthProvider'
 import FavMovieCard from '../../components/moviesComponents/FavMovieCard'
-import NoFavourite from '../../components/NoFavourite'
+
 
 const FavouriteMovies = () => {
   const {user} = useContext(AuthContext) 
@@ -18,12 +18,11 @@ const FavouriteMovies = () => {
     <div className='w-11/12 mx-auto my-10 space-y-10'>
       <h2 className="text-3xl font-semibold text-center dark:text-white">Favourite Movies</h2>
       {
-        favMovies.length?<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {
-           favMovies.map(movie=><FavMovieCard  key={movie._id} favMovies={favMovies} setFavMovies={setFavMovies} movie={movie}/>)
-        }
-      </div>:
-      <NoFavourite/>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+       {
+          favMovies.map(movie=><FavMovieCard  key={movie._id} favMovies={favMovies} setFavMovies={setFavMovies} movie={movie}/>)
+       }
+     </div>
       }
     </div>
   )
